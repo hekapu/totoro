f7818e27ac058bc806cef8ea0dccfe9f15773248
Agafonov Andrew <pentri4@gmail.com>
2020-03-20 17:10:08 +0700

    Update site architecture

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../theme/App.css';
 
-
 function App() {
   return (
     <div className="App">
-
+      <Header/>
+      <mainContent/>
+      <footer/>
     </div>
   );
 }
